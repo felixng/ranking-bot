c50aa08020c47c43e3a2145ddd5f1689e9e6dcee
felix.ng <nfelix@gmail.com>
2017-02-26 22:27:58 +0000

    clone from day seater bot

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'E8f6pjzHmEwkafHKIDnSQ45U6',
+  consumer_secret:      '5at3hawGgtXsIITcSGQZDSJuVbREaOUMFnLaFzB88iP5VZazKu',
+  access_token:         '764425026640764933-gj9aAv4DjvrGRNkCrpkXbVePDNSA9ih',
+  access_token_secret:  'opt8o33t6NIN65iDmG8X2WLHICtNemhmhagJMRjh4Q3wX'
+}

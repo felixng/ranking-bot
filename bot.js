52a9f791c84ca4f40b6b09c8189415b2ff6a307f
felix.ng <nfelix@gmail.com>
2017-03-29 22:53:51 +0100

    Fix configs, add init crawl

--- bot.js
+++ bot.js
@@ -1,14 +1,31 @@
 require('newrelic');
 var TweetCounter = require('./tweetCounter.js');
-var debug = false;
+var debug = process.env.DEBUG | false;
 
 var Twit = require('twit');
 var redis = require('redis').createClient(process.env.REDIS_URL || "redis://h:p681205353c4df3fcd2ac99172b553019835bd15ea1a943fb759dc3c5ac344aa9@ec2-34-251-82-220.eu-west-1.compute.amazonaws.com:16849");
-var T = new Twit(require('./config.js'));
-var counter = new TweetCounter(T, redis);
 
-// setInterval(retweetLatest, 1000 * 60 * 12);1
-// counter.gatherAll();
+if (debug){
+    var T = new Twit(require('./localconfig.js'));
+}
+else{
+    var T = new Twit(require('./config.js'));
+}
+    
+//var counter = new TweetCounter(T, redis);
+
+
+var index = 30;
+
+initGather();
+setInterval(initGather, 1000 * 60 * 20);
+
+function initGather(){
+    var counter = new TweetCounter(T, redis, index);
+    index = index - 1;
+    
+    counter.gatherAll();
+}
 
 var express = require('express');
 var app = express();
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         process.env.CONSUMER_KEY | 'E8f6pjzHmEwkafHKIDnSQ45U6',
-  consumer_secret:      process.env.CONSUMER_SECRET | '5at3hawGgtXsIITcSGQZDSJuVbREaOUMFnLaFzB88iP5VZazKu',
-  access_token:         process.env.ACCESS_TOKEN | '764425026640764933-gj9aAv4DjvrGRNkCrpkXbVePDNSA9ih',
-  access_token_secret:  process.env.ACCESS_TOKEN_SECRET | 'opt8o33t6NIN65iDmG8X2WLHICtNemhmhagJMRjh4Q3wX'
+  consumer_key:         process.env.CONSUMER_KEY,
+  consumer_secret:      process.env.CONSUMER_SECRET,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 }

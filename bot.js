09dcea1bbed574deac9d64fd896c2e0d38c4ecb5
felixn <felix.ng@thetrainline.com>
2017-05-26 14:18:47 +0100

    change back to config..

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ var Twit = require('twit');
 var redis = require('redis').createClient(process.env.REDIS_URL || "redis://h:pc8d86cd65c7102d817057135e85213ccdb2e7196eda9a558c96f7b53cbb0ad18@ec2-34-249-251-118.eu-west-1.compute.amazonaws.com:13119");
 var westEndSheet = process.env.WESTEND_SHEET || 'id=1GsNXv7Na24WB5XzKCKlHl_72GLAxOrs9K_v2sYQ4eQ4&sheet=1';
 var trainSheet = process.env.TRAIN_SHEET || 'id=1GsNXv7Na24WB5XzKCKlHl_72GLAxOrs9K_v2sYQ4eQ4&sheet=2';
-var T = new Twit(require('./localconfig.js'));
+var T = new Twit(require('./config.js'));
 
 var counter = new TweetCounter('', T, redis, westEndSheet);
 var trainCounter = new TweetCounter('Train', T, redis, trainSheet);

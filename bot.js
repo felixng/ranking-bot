2d6eb8c025eeb71efa3dedb42b4128c9ac45dfe9
felix.ng <nfelix@gmail.com>
2017-05-27 15:03:40 +0100

    added auth

--- bot.js
+++ bot.js
@@ -1,12 +1,25 @@
 require('newrelic');
 var TweetCounter = require('./tweetCounter.js');
 var CronJob = require('cron').CronJob;
-var debug = process.env.DEBUG;
+var passport = require('passport');
+var Strategy = require('passport-http').BasicStrategy;
+var db = require('./db');
 
+var debug = process.env.DEBUG;
 var Twit = require('twit');
 var redis = require('redis').createClient(process.env.REDIS_URL || "redis://h:pc8d86cd65c7102d817057135e85213ccdb2e7196eda9a558c96f7b53cbb0ad18@ec2-34-249-251-118.eu-west-1.compute.amazonaws.com:13119");
 var westEndSheet = process.env.WESTEND_SHEET || 'id=1GsNXv7Na24WB5XzKCKlHl_72GLAxOrs9K_v2sYQ4eQ4&sheet=1';
 var trainSheet = process.env.TRAIN_SHEET || 'id=1GsNXv7Na24WB5XzKCKlHl_72GLAxOrs9K_v2sYQ4eQ4&sheet=2';
+
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
 
 var configPath = './config.js';
 if (debug == null || !debug){
@@ -64,6 +77,10 @@ app.post('/crawl/:daysAgo/:length', function(req, res) {
 	counter.gatherAllDuration(req.params.daysAgo, req.params.length);
 	
 	res.status(200).send();
+});
+
+app.get('/felix', passport.authenticate('basic', { session: false }), function(req, res) {
+	res.status(200).send('Hello Felix! :) ' + req.headers['authorization']);
 });
 
 var port = process.env.PORT || 1337;

587453d8de623baaa8eb4666c8e9f6aff8443b33
felix.ng <nfelix@gmail.com>
2017-07-22 16:50:45 +0100

    updated title style

--- app/containers/Cloud/Title.js
+++ app/containers/Cloud/Title.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import H2 from '../../components/H2';
 
 const Title = styled.h2`
-	margin-top: 1.5em;
+	padding-top: 1.5em;
 	margin-bottom: 1.5em;
 	font-size: 2em;
 `;

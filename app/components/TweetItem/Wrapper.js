bddbdd0ca75c54efc3047becd078d9e7634029db
felix.ng <nfelix@gmail.com>
2017-08-05 15:09:25 +0100

    align spacing

--- app/components/TweetItem/Wrapper.js
+++ app/components/TweetItem/Wrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
 	display: inline-block;
 	vertical-align: top;
-	padding: 0.5em;
+	padding: 0 0.5em;
 
 	-webkit-transform: translateZ(0);
     -moz-transform: translateZ(0);
